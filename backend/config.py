b4e80c6b8f0fd2f9370e450ffd4bb0a3683a5eea
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-04-15 20:28:03 +0000

    feat: add config.py
    Contains configuration files for the flask app and database

---
+++ backend/config.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_cors import CORS
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+app = Flask(__name__)
+
+# Initializes Cross Origin Resource sharing for the application. without it couldnt access different pages. Might want to ensure CSRF on the frontend forms
+CORS(app)
+
+# Set up Flask app to access database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///soko.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+ # Create SQLALchemy engine to access database
+engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+
+# Session maker binds any sessions created to the chosen db
+Session = sessionmaker(bind=engine)
+
+# Create a session
+session = Session()
+
+# Base calss for SQLAlchemy models
+Base = declarative_base()
