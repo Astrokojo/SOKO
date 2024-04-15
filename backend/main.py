c6eb52f3aa120a00645fa16772af7264e62a57a1
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-04-15 20:28:42 +0000

    feat: add  main.py
    file from which to start the flask app

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from config import app, Base, engine
+from Product.routes import product_bp, create_sample
+from Cart.routes import cart_bp
+
+# Import the blueprints
+app.register_blueprint(product_bp)
+app.register_blueprint(cart_bp)
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        create_sample()
+        Base.metadata.create_all(engine)
+    
+    app.run(host='0.0.0.0', port=8080)
